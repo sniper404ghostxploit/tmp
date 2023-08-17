ff91cef1db1809db51f9bf55af3fa3ad25b3cbbc
varwwwhtmlsc <141085403+varwwwhtmlsc@users.noreply.github.com>
2023-08-17 15:04:48 +0700

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+counter=1
+while [ $counter -le 31536000 ]
+chmod 0555 /home/storm/sites/acmilananalysis-com/public/wp-content/uploads/2019/11 -R
+do
+	echo $counter
+	((counter++))
+done
